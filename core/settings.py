9592f3e999aa4c396179f70459fb1272d3ed17f9
Carlos Lazaro <carlos.e.lazaro@hotmail.com>
2024-05-21 20:21:04 -0300

    Ampliación campos modelo User

--- core/settings.py
+++ core/settings.py
@@ -134,3 +134,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+AUTH_USER_MODEL = 'user.User'
--- user/models.py
+++ user/models.py
@@ -1,4 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+        imagen = models.ImageField( default='perfil_default.png', upload_to='users/' )
+        ocupacion = models.CharField( max_length=150, null=True, blank=True )
+        direccion = models.CharField( max_length=150, null=True, blank=True )
+        localidad = models.CharField( max_length=100, null=True, blank=True )
+        provincia = models.CharField( max_length=100, null=True, blank=True )
+        pais = models.CharField( max_length=100, null=True, blank=True )
+        telefono = models.CharField( max_length=100, null=True, blank=True )
+        bio = models.TextField( null=True, blank=True )
